8f372b7f7071b04431d0740067cfedcb0fb637c0
dimshiusua <dimsiusua@gmail.com>
2017-02-02 21:29:18 +0200

    карусль готова

--- app/js/init.js
+++ app/js/init.js
@@ -4,4 +4,6 @@ $('.carousel').slick({
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
+  prevArrow: '<button type="button" class="slick-prev-arrow"><img src="img/arrow-prev.png"></button>',
+  nextArrow: '<button type="button" class="slick-next-arrow"><img src="img/arrow-next.png"></button>',
 });
