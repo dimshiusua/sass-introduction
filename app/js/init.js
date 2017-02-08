25f129bbb1b38fe605c5c1e2cf9b870e94dad781
dimshiusua <dimsiusua@gmail.com>
2017-02-08 13:34:27 +0200

    подключен form styler

--- app/js/init.js
+++ app/js/init.js
@@ -7,3 +7,15 @@ $('.carousel').slick({
   prevArrow: '<button type="button" class="slick-prev-arrow"><img src="img/arrow-prev.png"></button>',
   nextArrow: '<button type="button" class="slick-next-arrow"><img src="img/arrow-next.png"></button>',
 });
+//form styler init
+(function($) {
+$(function() {
+
+  $('select').styler();
+
+});
+})(jQuery);
+//form styler delay to correct fonts errors (http://dimox.name/jquery-form-styler/ )
+setTimeout(function() {
+  $('input, select').styler();
+}, 100)
