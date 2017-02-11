f3cdb0c513d35f0fa198df849ffd7d51891378c0
dimshiusua <dimsiusua@gmail.com>
2017-02-11 18:05:03 +0200

    @media 961px

--- app/js/init.js
+++ app/js/init.js
@@ -1,11 +1,22 @@
 
 $('.carousel').slick({
-  slidesToShow: 8,
+  slidesToShow:6,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
   prevArrow: '<button type="button" class="slick-prev-arrow"><img src="img/arrow-prev.png"></button>',
   nextArrow: '<button type="button" class="slick-next-arrow"><img src="img/arrow-next.png"></button>',
+  centerMode: true,
+  centerPadding: '0px',
+  pauseOnHover: true,
+  responsive: [
+    {
+      breakpoint: 1081,
+      settings:{
+        slidesToShow:4,
+      }
+    }
+  ]
 });
 //form styler init
 (function($) {
